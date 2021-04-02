4720706929f68b687b64ac7d6f346fc31612dec9
Atif <ec20229@qmul.ac.uk>
2021-04-02 16:13:44 +0100

    changed

--- webroot/phase1/homepage.php
+++ webroot/phase1/homepage.php
@@ -1,4 +1,4 @@
-homepage.php<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<hgroup>
 			<title>Portfolio Homepage</title>
